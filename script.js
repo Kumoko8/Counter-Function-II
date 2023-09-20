9ab47d708a539091654be21d172f208b987707b6
Taylor Golden <131223690+Kumoko8@users.noreply.github.com>
2023-09-20 12:54:53 -0500

    Create script.js and base structure for counter function

---
+++ script.js
@@ -0,0 +1,19 @@
+//the function create counter will return an objec with three functions, one to increment, one to decrement and one to reset the intial integer passed
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    return {
+      
+    }
+      
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
