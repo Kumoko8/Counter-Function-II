dce1ffeb295a92315c4cb788175dc3da78d5361a
Kumori <tauen8@gmail.com>
2023-09-23 15:27:30 -0500

    completed integer counting functionality

--- script.js
+++ script.js
@@ -5,15 +5,31 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
+    const original = init
     return {
+      increment: function () {
+        return ++init
+      },
+      reset: function () {
+       
+        return init = original
+      },
+      decrement: function () {
+        return --init
+      },
       
     }
       
 };
 
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
+
+ const counter = createCounter(5)
+//  counter.increment(); // 6
+//  counter.reset(); // 5
+//  counter.decrement(); // 4
+
+ console.log(counter.increment());
+ console.log(counter.reset());
+ console.log(counter.decrement());
+ 
+
